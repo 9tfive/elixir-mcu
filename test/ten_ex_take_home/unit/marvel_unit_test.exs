12b971bf4911830bb8c0a45233bd4feb1f27dce5
mughis369 <mughis.mirza95@gmail.com>
2023-07-16 02:24:02 +0500

    remove links from header and remove compile warning

--- test/ten_ex_take_home/unit/marvel_unit_test.exs
+++ test/ten_ex_take_home/unit/marvel_unit_test.exs
@@ -3,10 +3,6 @@ defmodule TenExTakeHome.Api.MarvelTest do
 
   import Mox
 
-  alias TenExTakeHome.Api.Marvel
-  alias TenExTakeHome.Api.MarvelBehaviour
-
-
   test "fetches characters from the API" do
     TenExTakeHome.MockMarvelApi
     |> expect(:fetch_characters, fn _page -> {1, []} end)
